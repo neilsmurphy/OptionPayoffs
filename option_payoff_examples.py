cfcaf7c1b45313f6795f0bf9620f976f2752a375
Neil Murphy <neil@neilmurphy.tech>
2021-09-18 14:05:51 -0400

    Primarily created jupyter notebook. Added bear call spread to functions.

--- option_payoff_examples.py
+++ option_payoff_examples.py
@@ -1,4 +1,29 @@
 from option_payoffs import *
+import numpy as np
+
+
+def spread_example():
+    underlying_price = 146
+    strike_price_long = 147
+    premium_long = 2.15
+    strike_price_short = 150
+    premium_short = 1.40
+    right = "call"
+    range_multiple = 0.1
+
+    stock_price_range = np.arange(
+        (1 - range_multiple) * underlying_price, (1 + range_multiple) * underlying_price
+    )
+
+    return spreads(
+        underlying_price,
+        strike_price_long,
+        premium_long,
+        strike_price_short,
+        premium_short,
+        stock_price_range,
+        right,
+    )
 
 
 def iron_condor_long_example():
@@ -17,14 +42,22 @@ def iron_condor_long_example():
     strike_price_short_call = 110
     premium_short_call = 0.95
 
-    
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
-  
-    iron_condor_net =  iron_condor(underlying_price, strike_price_long_put, premium_long_put,
-                                  strike_price_short_put,  premium_short_put,
-                                  strike_price_short_call, premium_short_call,
-                                  strike_price_long_call,  premium_long_call,
-                                  stock_price_range, False)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
+
+    iron_condor_net = iron_condor(
+        underlying_price,
+        strike_price_long_put,
+        premium_long_put,
+        strike_price_short_put,
+        premium_short_put,
+        strike_price_short_call,
+        premium_short_call,
+        strike_price_long_call,
+        premium_long_call,
+        stock_price_range,
+        False,
+    )
+
 
 def iron_condor_short_example():
     underlying_price = 100
@@ -41,14 +74,22 @@ def iron_condor_short_example():
     strike_price_short_call = 105
     premium_short_call = 2.35
 
-    
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
-  
-    iron_condor_net =  iron_condor(underlying_price, strike_price_long_put, premium_long_put,
-                                  strike_price_short_put,  premium_short_put,
-                                  strike_price_short_call, premium_short_call,
-                                  strike_price_long_call,  premium_long_call,
-                                  stock_price_range, True)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
+
+    iron_condor_net = iron_condor(
+        underlying_price,
+        strike_price_long_put,
+        premium_long_put,
+        strike_price_short_put,
+        premium_short_put,
+        strike_price_short_call,
+        premium_short_call,
+        strike_price_long_call,
+        premium_long_call,
+        stock_price_range,
+        True,
+    )
+
 
 def long_strangle_example():
     underlying_price = 50
@@ -57,9 +98,17 @@ def long_strangle_example():
     strike_price_put = 48
     premium_put = 2.85
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    long_strangle_net = strangle_or_straddle(underlying_price, strike_price_call, premium_call, strike_price_put, premium_put, stock_price_range, False)
+    long_strangle_net = strangle_or_straddle(
+        underlying_price,
+        strike_price_call,
+        premium_call,
+        strike_price_put,
+        premium_put,
+        stock_price_range,
+        False,
+    )
 
 
 def short_strangle_example():
@@ -69,9 +118,17 @@ def short_strangle_example():
     strike_price_put = 48
     premium_put = 2.85
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    short_strangle_net = strangle_or_straddle(underlying_price, strike_price_call, premium_call, strike_price_put, premium_put, stock_price_range, True)
+    short_strangle_net = strangle_or_straddle(
+        underlying_price,
+        strike_price_call,
+        premium_call,
+        strike_price_put,
+        premium_put,
+        stock_price_range,
+        True,
+    )
 
 
 def long_straddle_example():
@@ -81,9 +138,18 @@ def long_straddle_example():
     strike_price_put = 15
     premium_put = 1
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    long_straddle_example = strangle_or_straddle(underlying_price, strike_price_call, premium_call, strike_price_put, premium_put, stock_price_range, False)
+    long_straddle_example = strangle_or_straddle(
+        underlying_price,
+        strike_price_call,
+        premium_call,
+        strike_price_put,
+        premium_put,
+        stock_price_range,
+        False,
+    )
+
 
 def short_straddle_example():
     underlying_price = 15
@@ -92,10 +158,18 @@ def short_straddle_example():
     strike_price_put = 15
     premium_put = 1
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    short_straddle_example = strangle_or_straddle(underlying_price, strike_price_call, premium_call, strike_price_put, premium_put, stock_price_range, True)
-    
+    short_straddle_example = strangle_or_straddle(
+        underlying_price,
+        strike_price_call,
+        premium_call,
+        strike_price_put,
+        premium_put,
+        stock_price_range,
+        True,
+    )
+
 
 def call_debit_spread_example():
     underlying_price = 49
@@ -104,9 +178,18 @@ def call_debit_spread_example():
     strike_price_short = 60
     premium_short = 1
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    cds = call_credit_or_debit_spread(underlying_price, strike_price_long, premium_long, strike_price_short, premium_short, stock_price_range, False)
+    cds = call_credit_or_debit_spread(
+        underlying_price,
+        strike_price_long,
+        premium_long,
+        strike_price_short,
+        premium_short,
+        stock_price_range,
+        False,
+    )
+
 
 def put_debit_spread_example():
     underlying_price = 30
@@ -115,9 +198,18 @@ def put_debit_spread_example():
     strike_price_short = 30
     premium_short = 1.75
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    pds = put_credit_or_debit_spread(underlying_price, strike_price_long, premium_long, strike_price_short, premium_short, stock_price_range, False)
+    pds = put_credit_or_debit_spread(
+        underlying_price,
+        strike_price_long,
+        premium_long,
+        strike_price_short,
+        premium_short,
+        stock_price_range,
+        False,
+    )
+
 
 def call_credit_spread_example():
     underlying_price = 240
@@ -126,9 +218,18 @@ def call_credit_spread_example():
     strike_price_long = 245
     premium_long = 2
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    cds_credit = call_credit_or_debit_spread(underlying_price, strike_price_long, premium_long, strike_price_short, premium_short, stock_price_range, True)
+    cds_credit = call_credit_or_debit_spread(
+        underlying_price,
+        strike_price_long,
+        premium_long,
+        strike_price_short,
+        premium_short,
+        stock_price_range,
+        True,
+    )
+
 
 def put_credit_spread_example():
     underlying_price = 30
@@ -137,9 +238,17 @@ def put_credit_spread_example():
     strike_price_long = 28
     premium_long = 0.30
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    pds_credit = put_credit_or_debit_spread(underlying_price, strike_price_long, premium_long, strike_price_short, premium_short, stock_price_range, True)
+    pds_credit = put_credit_or_debit_spread(
+        underlying_price,
+        strike_price_long,
+        premium_long,
+        strike_price_short,
+        premium_short,
+        stock_price_range,
+        True,
+    )
 
 
 def call_example(short=False, plot=True):
@@ -147,16 +256,20 @@ def call_example(short=False, plot=True):
     strike_price = 50
     premium = 2
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    call = call_option(underlying_price, strike_price, premium, stock_price_range, short, plot)
-    
+    call = call_option(
+        underlying_price, strike_price, premium, stock_price_range, short, plot
+    )
+
+
 def put_example(short=False, plot=True):
     underlying_price = 49
     strike_price = 50
     premium = 2
 
-    stock_price_range = np.arange(0.50*underlying_price, 1.50*underlying_price)
+    stock_price_range = np.arange(0.50 * underlying_price, 1.50 * underlying_price)
 
-    put = put_option(underlying_price, strike_price, premium, stock_price_range, short, plot)
-
+    put = put_option(
+        underlying_price, strike_price, premium, stock_price_range, short, plot
+    )
